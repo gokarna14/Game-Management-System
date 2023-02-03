1c63f9cb90b1c0d34dd7bbc6138f827cf4bfd28d
System Administrator <gokarna.adhikari14@gmail.com>
2023-02-04 00:17:55 +0545

    Logic for delete needs to be re-watched

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -96,5 +96,49 @@ namespace GameManagementSystem.Controllers
             }
             return View( );
         }
+
+         // GET
+        public IActionResult Delete(int? id)
+        {
+                System.Console.WriteLine(id + "\n\n\n");
+            
+            if(id == null || id <= 0)
+            {
+                return NotFound();
+            }
+            var adminFromDb = _db.Admin.Find(id);
+
+            if (adminFromDb == null)        
+            {
+                return NotFound();
+            }
+
+            return View(adminFromDb);
+        }
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Admin obj)
+        {
+
+            System.Console.WriteLine(obj.LastName + "\n\n\n");
+            if (obj.FirstName == obj.Username )
+            {
+                ModelState.AddModelError("FirstName", "The first name  and username cannot be same.");
+            }
+            if (!Regex.IsMatch(obj.FirstName + obj.MiddleName + obj.LastName,@"^[a-zA-Z]+$"))
+            {
+                ModelState.AddModelError("CustomError", "The name field cannot include numbers or special characters");
+            } 
+
+            if (ModelState.IsValid)
+            {    
+                // obj.Updated = DateTime.Now;
+                _db.Admin.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View( );
+        }
     }
 }
